5bbeb784f88cd3a56e21e98da7bc1c7dea9800e2
Ali Amin <aliamin588@gmail.com>
2023-04-17 06:37:11 +0500

    db support added

--- src/pages/properties/index.tsx
+++ src/pages/properties/index.tsx
@@ -1,7 +1,19 @@
+
+   
+
 export default function Property() {
     return (
-        <div>
-            Property Page
+        <div className="grid grid-cols-12">
+            <div className="col-span-2">
+                <div className="border-solid border-2 border-gray-300 rounded p-4">
+                    Filters will be here
+                </div>
+            </div>
+            <div className="col-span-10">
+                <div className="p-4">
+
+                </div>
+            </div>
         </div>
     )
 }
