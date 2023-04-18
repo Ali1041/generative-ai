e046ae62ea35289011c3d9e27ec81c792982ff98
Ali Amin <aliamin588@gmail.com>
2023-04-18 06:56:47 +0500

    CRUD operation started

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -36,12 +36,12 @@ export const Header = () => {
           </span>
         </a>
         <div className="flex md:order-2">
-          <button
+          <Link href={"/create-property"}
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Get started
-          </button>
+          </Link>
           <button
             data-collapse-toggle="navbar-cta"
             type="button"
